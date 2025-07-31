a025d53e76c8fa0f2d147fb75b4214922deabb3c
Mukesh <mukesh.sah@msg-global.com>
2025-07-31 08:37:45 +0530

    first commit

---
+++ constants.py
@@ -0,0 +1,9 @@
+# Configuration and constants used in the script. Update values as needed.
+
+USERNAME = "mukeshsah1405@gmail.com"
+PASSWORD = "P@ssword@7668"
+MOBILE = "9632432862"
+ORIGINAL_RESUME_PATH="your-resume-file-path/Resume.pdf"
+MODIFIED_RESUME_PATH="modified-resume-file-path/Resume.pdf"
+NAUKRI_LOGIN_URL="https://www.naukri.com/nlogin/login"
+NAUKRI_PROFILE_URL="https://www.naukri.com/mnjuser/profile"
